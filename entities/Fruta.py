a7c308197ae82dd170248ff51546247508a9c3d5
Douglas Holanda <douglasholanda3195@gmail.com>
2023-09-08 09:38:26 -0300

    Created package of entities

---
+++ entities/Fruta.py
@@ -0,0 +1,9 @@
+from entities.Mercadoria import Mercadoria
+
+
+class Fruta(Mercadoria):
+    def __init__(self, nome, codigo, quantidade, preco):
+        super().__init__(nome, codigo, quantidade, preco)
+
+        def EuCalculo(self):
+            return self._preco * self._quantidade
---
+++ entities/Mercadoria.py
@@ -0,0 +1,51 @@
+from repositories.EuCalculo import EuCalculo
+
+
+class Mercadoria(EuCalculo):
+    def __init__(self, nome, codigo, quantidade, preco):
+        self._nome = nome
+        self._codigo = codigo
+        self._quantidade = quantidade
+        self._preco = preco
+
+        @property
+        def nome(self):
+            return self._nome
+        @nome.setter
+        def nome(self, nome):
+            self._nome = nome
+
+        @property
+        def codigo(self):
+            return self._codigo
+
+        @nome.setter
+        def codigo(self, codigo):
+            self._codigo = codigo
+
+        @property
+        def quantidade(self):
+            return self._quantidade
+
+        @quantidade.setter
+        def quantidade(self, quantidade):
+            self._quantidade = quantidade
+
+        @property
+        def preco(self):
+            return self._preco
+
+        @preco.setter
+        def preco(self, preco):
+            self._preco = preco
+
+        def EuCalculo(self):
+            return self._preco*self._quantidade
+
+
+
+
+
+
+
+
